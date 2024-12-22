666eea510af887ef3823ed43261c976c63210dd5
J Gynn <48025294+j-gynn@users.noreply.github.com>
2024-12-22 23:51:02 +0000

    formatting

--- scripts/cat/cats.py
+++ scripts/cat/cats.py
@@ -3450,12 +3450,14 @@ class Cat:
                 "favourite": self.favourite,
             }
 
-    def determine_next_and_previous_cats(self, filter_func: Callable[[Cat], bool] = None):
+    def determine_next_and_previous_cats(
+        self, filter_func: Callable[[Cat], bool] = None
+    ):
         """Determines where the next and previous buttons point to, relative to this cat.
 
         :param status: Allows you to constrain the list by status
-        :param filter_func: Allows you to constrain the list by any attribute of 
-            the Cat object. Takes a function which takes in a Cat instance and 
+        :param filter_func: Allows you to constrain the list by any attribute of
+            the Cat object. Takes a function which takes in a Cat instance and
             returns a boolean.
         """
         sorted_specific_list = [
