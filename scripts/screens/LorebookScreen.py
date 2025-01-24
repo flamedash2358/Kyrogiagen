d62b72636ed24397881ddfa687b4fb99e82b10e8
flamedash2358 <lucykooistra@icloud.com>
2025-01-24 11:37:45 -0800

    Update LorebookScreen.py

--- scripts/screens/LorebookScreen.py
+++ scripts/screens/LorebookScreen.py
@@ -1,1 +1,10 @@
+from random import choice, randrange
+from re import sub
+from typing import Optional
 
+import i18n
+import pygame
+import pygame_gui
+from pygame_gui.core import ObjectID
+
+import scripts.screens.screens_core.screens_core
